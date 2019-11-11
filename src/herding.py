176d29a7e55d0e67119315bed409d0d87c926e0d
Tom271 <trademarkhodgson@gmail.com>
2019-11-11 17:33:47 +0000

    Update Files 1

--- src/herding.py
+++ src/herding.py
@@ -4,13 +4,13 @@ from scipy.integrate import simps
 
 def M1_hom(f_t, v):
     dv = v[1] - v[0]
-    #Simpson's Rule
-    avg_v =  simps(v*f_t, dx=dv)
+    # Simpson's Rule
+    avg_v = simps(v*f_t, dx=dv)
     return avg_v
 
 def Mn(f_t, v, n):
     dv = v[1] - v[0]
-    #Simpson's Rule
+    # Simpson's Rule
     avg_v =  simps((v**n)*f_t, dx=dv)
 
     return avg_v
