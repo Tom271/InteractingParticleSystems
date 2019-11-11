b8b80daf23bace72dfe9dfe35b487d474c306b45
Tom271 <trademarkhodgson@gmail.com>
2019-11-11 18:47:03 +0000

    any colour as long as its black

--- src/herding.py
+++ src/herding.py
@@ -2,21 +2,23 @@ import numpy as np
 from math import atan
 from scipy.integrate import simps
 
+
 def M1_hom(f_t, v):
     dv = v[1] - v[0]
     # Simpson's Rule
-    avg_v = simps(v*f_t, dx=dv)
+    avg_v = simps(v * f_t, dx=dv)
     return avg_v
+
 
 def Mn(f_t, v, n):
     dv = v[1] - v[0]
     # Simpson's Rule
-    avg_v =  simps((v**n)*f_t, dx=dv)
+    avg_v = simps((v ** n) * f_t, dx=dv)
 
     return avg_v
 
 
 if __name__ == "__main__":
-    #TODO Put tests here, plots of functions?
-    y = 'Hello World'
+    # TODO Put tests here, plots of functions?
+    y = "Hello World"
     print(y)
