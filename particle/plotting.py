1fffbaba8fc96ef01c13fe124add92e279bc6815
Tom271 <trademarkhodgson@gmail.com>
2020-01-09 11:56:34 +0000

    Functionalise and tidy up. Still need to add more line plots and maybe implement blitting?

--- particle/plotting.py
+++ particle/plotting.py
@@ -11,22 +11,6 @@ sns.set()
 sns.color_palette("colorblind")
 
 
-# def Q(x, phi, L=(2 * np.pi)):
-#     """ Calculate order parameter of Wang et al. 2017"""
-#     N = len(x)
-#     for particle, position in enumerate(x):
-#         distance = np.abs(x - position)
-#         distance = (np.minimum(distance, L - distance))
-#
-#     return order_parameter
-#
-#
-# def plot_order_parameter(x, phi, L):
-#     """ Plot order parameter of Wang et al. 2017"""
-#
-#     return
-
-
 def CL2(x, L=(2 * np.pi)):
     """Centered L2 discrepancy
     Adapted from https://stackoverflow.com/questions/50364048/
@@ -165,7 +149,18 @@ def anim_pos_vel_hist(
     return ani
 
 
-def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
+def anim_torus(
+    t,
+    _x,
+    v,
+    L=2 * np.pi,
+    mu_v=1,
+    variance=np.sqrt(2),
+    framestep=1,
+    pos_panel=None,
+    vel_panel=None,
+    subsample=None,
+):
     x = (2 * np.pi / L) * _x  # Quick hack to rescale to circle.
     fig = plt.figure(figsize=(40, 10))
     fig.patch.set_alpha(0.0)
@@ -190,15 +185,23 @@ def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
     torus_ax.plot(np.cos(an), np.sin(an), "-", alpha=0.5)
     torus_ax.axis("equal")
     fig.suptitle("t = {}".format(t[0]), fontsize=20)
-    ### Plotting particles on torus #####
+    # Plotting particles on torus
     torus_ax.set_ylim(-1.1, 1.1)
     torus_ax.set_xlim(-1.1, 1.1)
     torus_ax.set_facecolor("white")
 
-    pos_vel = x[0, v[0,] >= 0]
-    neg_vel = x[0, v[0,] < 0]
-    # pos_vel = pos_vel[:50]  # only plot this many particles for clearer anim
-    # neg_vel = neg_vel[:50]
+    if subsample:
+        # Take a random sample of points to plot on torus
+        subsample_mask = np.random.choice(len(x[0,]), size=subsample, replace=False)
+        x_subsample = x[:, subsample_mask]
+        v_subsample = v[:, subsample_mask]
+
+        pos_vel = x_subsample[0, v_subsample[0,] >= 0]
+        neg_vel = x_subsample[0, v_subsample[0,] < 0]
+    else:
+        pos_vel = x[0, v[0,] >= 0]
+        neg_vel = x[0, v[0,] < 0]
+
     (neg_points,) = torus_ax.plot(
         np.sin(neg_vel),
         np.cos(neg_vel),
@@ -217,20 +220,83 @@ def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
     )
     torus_ax.get_xaxis().set_visible(False)
     torus_ax.get_yaxis().set_visible(False)
+    if pos_panel == "line":
+        pos_lines = plot_pos_line(position_ax, position_time_ax, t, x)
+    else:
+        (
+            n_x,
+            bins_x,
+            patches_x,
+            n_x_time,
+            bins_x_time,
+            patches_x_time,
+        ) = plot_pos_hist(position_ax, position_time_ax, x)
 
-    #########################################
+    if vel_panel == "line":
+        vel_lines = plot_vel_line(vel_ax, vel_time_ax, t, x)
+        alala = 0
+    else:
+        (
+            n_v,
+            bins_v,
+            patches_v,
+            n_v_time,
+            bins_v_time,
+            patches_v_time,
+        ) = plot_vel_hist(vel_ax, vel_time_ax, v, mu_v, variance)
 
+    # fig.tight_layout()
+    fig.subplots_adjust(left=0.01, bottom=0.15)
+
+    def animate(i, framestep):
+
+        if subsample:
+            update_torus(
+                i, t, x_subsample, v_subsample, framestep, pos_points, neg_points,
+            )
+        else:
+            update_torus(i, t, x, v, framestep, pos_points, neg_points)
+
+        # update_pos_hist()  update_pos_line(subsample)
+        if pos_panel == "line":
+            update_pos_line(i, t, x, framestep, pos_lines)
+
+        else:
+            update_pos_hist(i, x, framestep, patches_x, patches_x_time)
+
+        # update_vel_hist() or update_vel_line(subsample)
+        if vel_panel == "line":
+            update_vel_line(i, t, v, framestep, vel_lines)
+
+        else:
+            update_vel_hist(i, v, framestep, patches_v, patches_v_time)
+
+        fig.suptitle("t = {:.2f}".format(t[i * framestep]), fontsize=20)
+        fig.show()
+
+    ani = animation.FuncAnimation(
+        fig, lambda i: animate(i, framestep), interval=60, frames=len(t) // framestep,
+    )
+
+    return ani
+
+
+def plot_vel_hist(vel_ax, vel_time_ax, v, mu_v, variance):
     # Plotting vel histogram
     n_v, bins_v, patches_v = vel_ax.hist(
         v[0,],
-        bins=np.arange(v.min(), v.max(), (v.max() - v.min()) / 30),
+        bins=np.arange(
+            min(-2, v.min()), max(2, v.max()), max(0.1, (v.max() - v.min()) / 30),
+        ),
         density=True,
         label="Velocity",
     )
 
     n_v_time, bins_v_time, patches_v_time = vel_time_ax.hist(
         v[0,],
-        bins=np.arange(v.min(), v.max(), (v.max() - v.min()) / 30),
+        bins=np.arange(
+            min(-2, v.min()), max(2, v.max()), max(0.1, (v.max() - v.min()) / 30),
+        ),
         density=True,
         label="Velocity",
     )
@@ -242,13 +308,16 @@ def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
     vel_ax.plot(_v, pde_stationary_dist, label=r"Stationary D$^{\mathrm{n}}$")
     vel_time_ax.plot(_v, pde_stationary_dist, label=r"Stationary D$^{\mathrm{n}}$")
     vel_ax.set_ylim(0, pde_stationary_dist.max() + 0.05)
-    vel_ax.set_xlim(v.min(), v.max())
-    # vel_ax.set_xlim(-1.1,1.1)
+    vel_ax.set_xlim(min(-2, v.min()), max(2, v.max()))
 
     vel_ax.set_ylabel("Density", fontsize=15)
     vel_time_ax.set_ylim(0, pde_stationary_dist.max() + 0.05)
-    vel_time_ax.set_xlim(v.min(), v.max())
+    vel_time_ax.set_xlim(min(-2, v.min()), max(2, v.max()))
 
+    return n_v, bins_v, patches_v, n_v_time, bins_v_time, patches_v_time
+
+
+def plot_pos_hist(position_ax, position_time_ax, x):
     # Plotting pos histogram
     n_x, bins_x, patches_x = position_ax.hist(
         x[0,],
@@ -264,10 +333,8 @@ def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
     )
 
     position_ax.set_ylim(0, 1.0)
-    # position_ax.set_xlim(x.min(), x.max())
     position_ax.set_xlim(0, 2 * np.pi)
     position_time_ax.set_ylim(0, 1.0)
-    # position_time_ax.set_xlim(x.min(), x.max())
     position_time_ax.set_xlim(0, 2 * np.pi)
 
     def format_func(value, tick_number):
@@ -291,63 +358,133 @@ def anim_full(t, _x, v, L=2 * np.pi, mu_v=1, variance=np.sqrt(2), framestep=1):
     position_time_ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 4))
     position_time_ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
 
-    mu_v = 1 / (2 * np.pi)
-
+    # PLot stationary distribution
+    mu_x = 1 / (2 * np.pi)
     _x = [x.min(), x.max()]
-    position_ax.plot(_x, [mu_v, mu_v], label=r"Stationary D$^{\mathrm{n}}$")
-    position_time_ax.plot(_x, [mu_v, mu_v], label=r"Stationary D$^{\mathrm{n}}$")
+    position_ax.plot(_x, [mu_x, mu_x], label=r"Stationary D$^{\mathrm{n}}$")
+    position_time_ax.plot(_x, [mu_x, mu_x], label=r"Stationary D$^{\mathrm{n}}$")
 
     position_ax.set_ylabel("Density", fontsize=15)
-    # fig.tight_layout()
-    fig.subplots_adjust(left=0.01, bottom=0.15)
 
-    def animate(i, framestep):
-        # Particles
-        pos_vel = x[i * framestep, v[i * framestep,] >= 0]
-        neg_vel = x[i * framestep, v[i * framestep,] < 0]
-        # pos_vel = pos_vel[:50]
-        # neg_vel = neg_vel[:50]
-        pos_points.set_data(np.sin(pos_vel), np.cos(pos_vel))
-        neg_points.set_data(np.sin(neg_vel), np.cos(neg_vel))
-        ####
+    return n_x, bins_x, patches_x, n_x_time, bins_x_time, patches_x_time
 
-        n_v, _ = np.histogram(
-            v[i * framestep,].flatten(),
-            bins=np.arange(v.min(), v.max(), (v.max() - v.min()) / 30),
-            density=True,
-        )
-        n_x, _ = np.histogram(
-            x[i * framestep,],
-            bins=np.arange(x.min(), x.max(), np.pi / 30),
-            density=True,
-        )
 
-        n_v_time, _ = np.histogram(
-            v[: i * framestep,].flatten(),
-            bins=np.arange(v.min(), v.max(), (v.max() - v.min()) / 30),
-            density=True,
-        )
-        n_x_time, _ = np.histogram(
-            x[: i * framestep,],
-            bins=np.arange(x.min(), x.max(), np.pi / 30),
-            density=True,
-        )
-        # Update vel data
-        for rect_v, height_v in zip(patches_v, n_v):
-            rect_v.set_height(height_v)
-        for rect_v_time, height_v_time in zip(patches_v_time, n_v_time):
-            rect_v_time.set_height(height_v_time)
-        # Update pos data
-        for rect_x, height_x in zip(patches_x, n_x):
-            rect_x.set_height(height_x)
-        for rect_x_time, height_x_time in zip(patches_x_time, n_x_time):
-            rect_x_time.set_height(height_x_time)
+def plot_pos_line(position_ax, position_time_ax, t, x):
+    pos_lines = []
+    for index in range(len(x[0,])):
+        lobj = position_ax.plot([], [], lw=1, alpha=0.1)[0]
+        pos_lines.append(lobj)
+    avg_line = position_ax.plot([], [], lw=2, alpha=1)[0]
+    pos_lines.append(avg_line)
 
-        fig.suptitle("t = {:.2f}".format(t[i * framestep]), fontsize=20)
-        fig.show()
+    position_ax.set_ylim(0, 2 * np.pi)
+    position_time_ax.set_ylim(0, 2 * np.pi)
+    position_ax.set_xlim(0, t[-1])
+    position_time_ax.set_xlim(0, t[-1])
+    return pos_lines
 
-    ani = animation.FuncAnimation(
-        fig, lambda i: animate(i, framestep), interval=60, frames=len(t) // framestep,
+
+def plot_vel_line(vel_ax, vel_time_ax, t, x):
+    vel_lines = []
+    for index in range(len(v[0,])):
+        lobj = vel_ax.plot([], [], lw=1, alpha=0.1)[0]
+        vel_lines.append(lobj)
+    avg_line = vel_ax.plot([], [], lw=2, alpha=1)[0]
+    vel_lines.append(avg_line)
+
+    vel_ax.set_ylim(min(-1.1, v.min()), max(1.1, v.max()))
+    vel_time_ax.set_ylim(-1.1, v.min()), max(1.1, v.max())
+    vel_ax.set_xlim(0, t[-1])
+    vel_time_ax.set_xlim(0, t[-1])
+    return vel_lines
+
+
+def update_torus(i, t, x, v, framestep, pos_points, neg_points):
+    # Update positions on torus
+    pos_vel = x[i * framestep, v[i * framestep,] >= 0]
+    neg_vel = x[i * framestep, v[i * framestep,] < 0]
+
+    pos_points.set_data(np.sin(pos_vel), np.cos(pos_vel))
+    neg_points.set_data(np.sin(neg_vel), np.cos(neg_vel))
+
+
+def update_pos_hist(i, x, framestep, patches_x, patches_x_time):
+    n_x, _ = np.histogram(
+        x[i * framestep,], bins=np.arange(x.min(), x.max(), np.pi / 30), density=True,
     )
 
-    return ani
+    n_x_time, _ = np.histogram(
+        x[: i * framestep,], bins=np.arange(x.min(), x.max(), np.pi / 30), density=True,
+    )
+
+    # Update pos data
+    for rect_x, height_x in zip(patches_x, n_x):
+        rect_x.set_height(height_x)
+    for rect_x_time, height_x_time in zip(patches_x_time, n_x_time):
+        rect_x_time.set_height(height_x_time)
+
+
+def update_vel_hist(i, v, framestep, patches_v, patches_v_time):
+    n_v, _ = np.histogram(
+        v[i * framestep,].flatten(),
+        bins=np.arange(
+            min(-2, v.min()), max(2, v.max()), max(0.1, (v.max() - v.min()) / 30),
+        ),
+        density=True,
+    )
+    n_v_time, _ = np.histogram(
+        v[: i * framestep,].flatten(),
+        bins=np.arange(
+            min(-2, v.min()), max(2, v.max()), max(0.1, (v.max() - v.min()) / 30),
+        ),
+        density=True,
+    )
+    # Update vel data
+    for rect_v, height_v in zip(patches_v, n_v):
+        rect_v.set_height(height_v)
+    for rect_v_time, height_v_time in zip(patches_v_time, n_v_time):
+        rect_v_time.set_height(height_v_time)
+    return
+
+
+def update_pos_line(i, t, x, framestep, pos_lines):
+    for lnum, line in enumerate(pos_lines[:-1]):
+        line.set_data(
+            t[: i * framestep], x[: i * framestep, lnum]
+        )  # set data for each line separately.
+    pos_lines[-1].set_data(t[: i * framestep], np.mean(x[: i * framestep,], axis=1))
+
+
+def update_vel_line(i, t, v, framestep, vel_lines):
+    for lnum, line in enumerate(vel_lines[:-1]):
+        line.set_data(
+            t[: i * framestep], v[: i * framestep, lnum]
+        )  # set data for each line separately.
+    vel_lines[-1].set_data(t[: i * framestep], np.mean(v[: i * framestep,], axis=1))
+
+
+if __name__ == "__main__":
+    import os
+    import pickle
+
+    cur_path = os.path.dirname(__file__)
+    test_data_path = os.path.relpath("..\\tests\\plot_test_data", cur_path)
+    # test_data_path = os.path.relpath(
+    #     "..\\Experiments\\1712Data\\ZeroNoiseDetIC_v_take2", cur_path
+    # )
+    test_data = pickle.load(open(test_data_path, "rb"))
+    t = test_data["Time"]
+    x = test_data["Position"]
+    v = test_data["Velocity"]
+    annie = anim_torus(
+        t,
+        x,
+        v,
+        mu_v=-1,
+        variance=1,
+        L=2 * np.pi,
+        framestep=1,
+        # pos_panel="line",
+        # vel_panel="line",
+    )
+    plt.show()
