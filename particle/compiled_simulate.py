6745cb73ec6219643e5abfb6761521bf5e89e2ae
Tom271 <trademarkhodgson@gmail.com>
2020-04-27 11:59:15 +0100

    Implementing jitclass

---
+++ particle/compiled_simulate.py
@@ -0,0 +1,93 @@
+"""
+Using jitclass on ParticleSystem()
+"""
+
+import numpy as np
+from numba import float32, int64  # import the types
+from numba import jitclass
+
+# Type everything!
+spec = [
+    ("particles", int64),  # a simple scalar field
+    ("L", float32),
+    ("T_end", float32),
+    ("dt", float32),
+    ("D", float32),
+    ("v0", float32[:]),  # a simple array field
+    ("x0", float32[:]),
+    ("x", float32[:, :]),
+    ("v", float32[:, :]),
+    ("N", int64),
+    ("interaction", float32[:]),
+]
+
+
+@jitclass(spec)
+class ParticleSystem:
+    def __init__(
+        self, particles=200, L=2 * np.pi, T_end=10, dt=0.01, D=0.5,
+    ):
+        self.particles = particles
+        self.L = L
+        self.T_end = T_end
+        self.dt = dt
+        self.D = D
+        self.v0 = np.ones(particles, dtype=np.float32)
+        # Numba won't allow all in one line
+        self.v0 *= 0.5
+        self.x0 = np.ones(particles, dtype=np.float32)
+
+        self.N = np.int64(self.T_end / self.dt)
+        self.x = np.zeros((self.N + 1, self.particles), dtype=np.float32)
+        self.v = np.zeros_like(self.x)
+
+    def calculate_interaction(self, x_curr, v_curr, gamma):
+        interaction_vector = np.zeros(len(x_curr))
+        for particle, position in enumerate(x_curr):
+            distance = np.abs(x_curr - position)
+            particle_interaction = np.less(
+                np.minimum(distance, self.L - distance), gamma * self.L
+            )
+            weighted_avg = np.sum(v_curr * particle_interaction) - v_curr[particle]
+            scaling = np.sum(particle_interaction) - 1 + 10 ** -15
+            interaction_vector[particle] = weighted_avg / scaling
+        return interaction_vector
+
+    def get_trajectories(self):
+        self.x[0] = self.x0
+        self.v[0] = self.v0
+        gamma = 0.1
+        for n in range(self.N):
+            interaction = self.calculate_interaction(self.x[n], self.v[n], gamma)
+            interaction = np.arctan(interaction) / np.arctan(1.0)
+            for point in range(len(self.x[n, :])):
+                self.x[n + 1, point] = (
+                    self.x[n, point] + self.v[n, point] * self.dt
+                ) % self.L  # Restrict to torus
+                self.v[n + 1, point] = (
+                    self.v[n, point]
+                    - (self.v[n, point] * self.dt)
+                    # + interaction[point] * self.dt
+                    + np.sqrt(2 * self.D * self.dt) * np.random.normal()
+                )
+        return self.x, self.v
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from datetime import datetime
+
+    startTime = datetime.now()
+    PS = ParticleSystem(particles=1000)
+    x, v = PS.get_trajectories()
+    print(f"Time Taken : {datetime.now()- startTime}")
+    plt.hist(x[0, :])
+    plt.show()
+    plt.hist(v[0, :])
+    plt.show()
+    print(PS.particles)
+    print(PS.L)
+    print(PS.T_end)
+    print(PS.dt)
+
+    print(PS.D)
