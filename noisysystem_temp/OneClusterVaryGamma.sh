22beb6041bfcceeab294617c231d59bb04d175b1
Tom271 <Tom271@users.noreply.github.com>
2021-04-02 13:39:25 +0100

    Create OneClusterVaryGamma.sh

---
+++ noisysystem_temp/OneClusterVaryGamma.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N MatchPDE
+#$ -cwd
+#$ -l h_rt=05:00:00
+#$ -l h_vmem=8G
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 600 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Matlab
+module load anaconda
+
+# Run the program
+source activate IPS
+python OneClusterVaryGamma.py
