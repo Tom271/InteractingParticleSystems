1cfc8b228077c2465e71d82cc288d713d3755392
Tom271 <Tom271@users.noreply.github.com>
2021-04-21 12:42:23 +0100

    Update OneClusterVaryGamma.py

--- noisysystem_temp/OneClusterVaryGamma.py
+++ noisysystem_temp/OneClusterVaryGamma.py
@@ -6,43 +6,43 @@ import particle.processing as processing
 # particles = [500]
 # gammas = np.concatenate(([0.01], np.arange(0.05, 0.2, 0.05)))
 # Match PDE Sim
-# cluster_width = 0.25
+cluster_width = 0.25
 #  np.array(
 #     [0.01, 0.05, 0.1, 0.15, 0.2]
 # )
-
-# test_params = {
-#     "particle_count": 100 * particles,  # (3 * np.arange(8, 150, 16)).tolist(),
-#     "gamma": gammas.tolist(),
-#     "G": ["Smooth"],
-#     "scaling": ["Local"],
-#     "D": [0.25],
-#     "phi": ["Gamma"],
-#     "initial_dist_x": ["wide_PDE_cluster"],
-#     "initial_dist_v": ["PDE_normal"],
-#     "T_end": [200.0],
-#     "dt": [0.005],
-#     "option": ["numba"],
-#     "record_time": [0.25],
-# }
-
-# cluster_width = 0.1
-# gammas = np.array([1 / 6, 1 / 3, 1 / 2, 2 / 3, 1, 4 / 3, 5]) * cluster_width
-
+gammas = np.array([1 / 6, 1 / 3, 1 / 2, 2 / 3, 1, 4 / 3, 2]) * cluster_width
 sim_parameters = {
-    "particle_count": [480],
-    "D": [0.25],
+    "particle_count": 100 * [2000],  # (3 * np.arange(8, 150, 16)).tolist(),
+    "gamma": gammas.tolist(),
     "G": ["Smooth"],
     "scaling": ["Global"],
-    "phi": ["Gamma"],
-    "gamma": np.arange(0.05, 0.55, 0.05).tolist(),
-    "initial_dist_x": ["one_cluster"],
-    "initial_dist_v": ["pos_normal_dn"],
+    "D": [0.25],
+    "phi": ["Normalised Gamma"],
+    "initial_dist_x": ["wide_PDE_cluster"],
+    "initial_dist_v": ["PDE_normal"],
     "T_end": [200.0],
     "dt": [0.005],
     "option": ["numba"],
     "record_time": [0.25],
 }
+
+# cluster_width = 0.1
+# gammas = np.array([1 / 6, 1 / 3, 1 / 2, 2 / 3, 1, 4 / 3, 5]) * cluster_width
+
+# sim_parameters = {
+#     "particle_count": [2000],
+#     "D": [0.25],
+#     "G": ["Smooth"],
+#     "scaling": ["Global"],
+#     "phi": ["Noramlised Gamma"],
+#     "gamma": np.arange(0.05, 0.55, 0.05).tolist(),
+#     "initial_dist_x": ["one_cluster"],
+#     "initial_dist_v": ["pos_normal_dn"],
+#     "T_end": [200.0],
+#     "dt": [0.005],
+#     "option": ["numba"],
+#     "record_time": [0.25],
+# }
 
 # history = processing.get_master_yaml(yaml_path="experiments_ran")
 # fn = (
