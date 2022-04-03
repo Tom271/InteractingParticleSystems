ac4f7f4a0556a86d06bfd529ee43a1f38816d4b4
Tom271 <trademarkhodgson@gmail.com>
2022-04-03 10:54:11 +0100

    Update PositivePhiAnalysis.py

--- noisysystem_temp/Analysis/PositivePhiAnalysis.py
+++ noisysystem_temp/Analysis/PositivePhiAnalysis.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 
-from particle.plotting import (
-    plot_averaged_convergence_from_clusters,
-    plot_averaged_avg_vel,
-    plot_avg_vel,
-)
+# from particle.plotting import (
+#     #plot_averaged_convergence_from_clusters,
+#     # plot_averaged_avg_vel,
+#     # plot_avg_vel,
+#     #multiple_timescale_plot,
+# )
+from particle.processing import get_main_yaml, get_parameter_range
+
+# from particle.statistics import calculate_avg_vel
+
 
 sns.set(style="white", context="talk")
 
@@ -30,24 +35,56 @@ elif os.name == "posix":
     os.chdir("/Volumes/Extreme SSD/InteractingParticleSystems/noisysystem_temp")
 
 yaml_path = "Experiments/positive_phi_no_of_clusters_high_noise_bump"
+history = get_main_yaml(yaml_path)
+clusters = get_parameter_range("initial_dist_x", history)
+
 fn = "_switch_"
 logged = False
 include_traj = True
-fig, [ax1, ax2] = plt.subplots(1, 2, figsize=(15, 5), sharex=True)
-ax2 = plot_avg_vel(ax2, search_parameters, logx=logged, exp_yaml=yaml_path)
-ax2 = plot_averaged_avg_vel(ax2, search_parameters, logx=logged, exp_yaml=yaml_path)
+fig, ax1 = plt.subplots(1, 1, figsize=(5, 5), sharex=True)
+# ax2 = plot_avg_vel(
+#     ax2,
+#     search_parameters,
+#     logx=logged,
+#     yaml_path=yaml_path,
+#     data_path="Experiments/parquet_data/",
+# )
+# ax2 = plot_averaged_avg_vel(
+#     ax2,
+#     search_parameters,
+#     logx=logged,
+#     yaml_path=yaml_path,
+#     data_path="Experiments/parquet_data/",
+# )
 
-ax1 = plot_averaged_convergence_from_clusters(
-    ax1, search_parameters, yaml_path, logx=logged
-)
-
-ax1.plot([0, search_parameters["T_end"]], [7.5, 7.5], "k--", alpha=0.2)
-ax2.set(xlabel="Time", ylabel=r"$M^N(t) $")
+# ax1 = plot_averaged_convergence_from_clusters(
+#     ax1,
+#     search_parameters,
+#     yaml_path,
+#     logx=logged,
+#     data_path="Experiments/parquet_data/",
+# )
+#
+# ax1.plot([0, 10], [7.5, 7.5], "k--", alpha=0.2)
+# ax2.set(xlabel="Time", ylabel=r"$M^N(t) $")
 # ax2.legend()
 # fig.savefig(f"img/PositivePhiClusters{fn}logged.jpg", dpi=300)
 # plt.tight_layout()
-plt.subplots_adjust(left=0.07, right=0.97, bottom=0.15, top=0.9, wspace=0.23)
+# plt.subplots_adjust(left=0.07, right=0.97, bottom=0.15, top=0.9, wspace=0.23)
+# plt.show()
+# metric_fn = calculate_avg_vel
+# fig2 = multiple_timescale_plot(
+#     search_parameters,
+#     break_time_step=40,
+#     metric=metric_fn,
+#     parameter="initial_dist_x",
+#     parameter_range=clusters,
+#     history=history,
+#     include_traj=True,
+#     data_path="Experiments/parquet_data/",
+# )
 plt.show()
+
 # logged = False
 # fig, [ax1, ax2] = plt.subplots(1, 2, figsize=(14, 5), sharex=True)
 # ax2 = plot_avg_vel(ax2, search_parameters, logx=logged, exp_yaml=yaml_path)
